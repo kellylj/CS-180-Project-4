83de3bfa49ae76518d7d44dcafb45da9e8664e36
liamkelly <kellylj@purdue.edu>
2021-11-04 15:55:55 -0400

    Added search methods to QuizManager

--- src/QuizManager.java
+++ src/QuizManager.java
@@ -8,11 +8,68 @@ public class QuizManager implements Manager {
 		this.lms = lms;
 	}
 
-
 	@Override
 	public void init() {
 		// TODO Auto-generated method stub
 
+	}
+
+	public void addQuiz(Quiz quiz) {
+		quizList.add(quiz);
+	}
+
+	public void removeQuiz() {
+
+	}
+
+	public String listQuizzes() {
+		String s = "";
+		for (Quiz q : quizList) {
+			s+= q.toString() + "\n";
+		}
+		if (quizList.size() == 0) {
+			s += "No quizzes have been created";
+		}
+		return s;
+	}
+
+	public String searchQuizByName(String name) {
+		String s = "";
+		for (Quiz q : quizList) {
+			if (q.getName().toLowerCase().contains(name.toLowerCase())) {
+				s += q.toString() + "\n";
+			}
+		}
+		if (s.equals("")) {
+			s+= "No quizzes found with that name";
+		}
+		return s;
+	}
+
+	public String searchQuizByAuthor(String author) {
+		String s = "";
+		for (Quiz q : quizList) {
+			if (q.getAuthor().toLowerCase().contains(author.toLowerCase())) {
+				s += q.toString() + "\n";
+			}
+		}
+		if (s.equals("")) {
+			s+= "No quizzes found with that author";
+		}
+		return s;
+	}
+
+	public String searchQuizByID(int ID) {
+		String s = "";
+		for (Quiz q : quizList) {
+			if (q.getID() == ID) {
+				s += q.toString() + "\n";
+			}
+		}
+		if (s.equals("")) {
+			s+= "No quizzes found with that ID";
+		}
+		return s;
 	}
 
 	@Override
