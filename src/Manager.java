d081b2fb13e8f4d8bb57db46ba6b32a14a804e99
AryanJain107 <jainaryan003@gmail.com>
2021-11-07 11:13:36 -0500

    Created the manager interface for the project

--- src/Manager.java
+++ src/Manager.java
@@ -1,8 +1,15 @@
-
+/**
+ * Manager interface for CS 180 Project 4
+ * All managers will come from this
+ * The methods of this interface will help users initialize anything
+ * before user does anything
+ *
+ * @author jain455
+ * @version 11/7/2021
+ */
 public interface Manager {
-
+	//Useful for if you need to initialize anything before user does anything.
 	public void init();
-	
+	//Make sure everything is saved to a file (if not automatically)!!!!
 	public void exit();
-	
 }
