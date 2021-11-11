66f4555968a7393b4110c253247aeb4e27a12066
Luderius <dxgeva@gmail.com>
2021-11-11 16:02:01 -0500

    Created GradedQuizFileManager

---
+++ src/main/GradedQuizFileManager.java
@@ -0,0 +1,18 @@
+package main;
+
+public class GradedQuizFileManager implements Manager {
+
+   LearningManagementSystem lms;
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void exit() {
+
+    }
+
+
+}
