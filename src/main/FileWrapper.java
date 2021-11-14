d462741fc7a908d0a9f558188a79edaa8b02cd0c
Luderius <dxgeva@gmail.com>
2021-11-13 22:01:18 -0500

    Commented FileWrapper, QuizFileManager, and UserFileManager

--- src/main/FileWrapper.java
+++ src/main/FileWrapper.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class FileWrapper {
 
-    public static ArrayList<String> readFile(String path) {
+    public static ArrayList<String> readFile(String path) { //reads a given file and returns an arraylist of strings, where one element corresponds to one line
         ArrayList<String> contents = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line;
@@ -21,7 +21,7 @@ public class FileWrapper {
         return contents;
     }
 
-    public static boolean writeFile(String path, ArrayList<String> contents) {
+    public static boolean writeFile(String path, ArrayList<String> contents) { //writes to a given file the contents of a given arraylist of strings where one element gets written to a line
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
             for (String line : contents) {
                 bw.write(line);
@@ -36,16 +36,16 @@ public class FileWrapper {
         return true;
     }
 
-    public static ArrayList<String> readImportFile(String path) {
+    public static ArrayList<String> readImportFile(String path) { //A version of readFile that is specific for importing quizzes in a file
         ArrayList<String> contents = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line;
             String readLine = "";
             while ((line = br.readLine()) != null) {
-                if (line.startsWith("/") && !readLine.isBlank()) {
+                if (line.startsWith("/") && !readLine.isBlank()) { //if the line starts with a "/" (forward slash) and the buffer (readLine) is not empty, the buffer is added to the arraylist and reset to the line
                     contents.add(readLine);
                     readLine = line.substring(1);
-                } else if (!line.startsWith("#")) {
+                } else if (!line.startsWith("#")) { //ignores any lines starting with "#" (pound), adds a read line to the buffer
                     readLine += line;
                 }
             }
