3ff6795bc5bf15bf12a94afa64fd5cf725e8f7ea
IsaacFleetwood <47401661+IsaacFleetwood@users.noreply.github.com>
2021-11-14 23:46:18 -0500

    Create Fake.java for voccheck

---
+++ Fake.java
@@ -0,0 +1,14 @@
+/**
+* A class used for making voccheck not give an error
+* when it is ran improperly. This class is not used
+* in the program and can be ignored.
+* 
+* Run run_voccheck.sh to verify that we pass
+* the coding style checks.
+* 
+* @author Nobody
+* @version 1.0.0
+*/
+public class Fake {
+  // Ignore this class. See the above javadoc for more info.
+}
