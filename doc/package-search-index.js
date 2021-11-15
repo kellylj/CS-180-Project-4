ad7b68f58f25ac2fc5dc2d07988e5a95052d0457
Isaac Fleetwood <ifleetwo@purdue.edu>
2021-11-15 16:13:53 -0500

    Update javadoc

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"main"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"main"},{"l":"ui"}];updateSearchResults();
