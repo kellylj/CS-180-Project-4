c0c09a7f5c2e27ce6e017003d65a33398050aa16
Isaac Fleetwood <ifleetwo@purdue.edu>
2021-11-15 14:49:32 -0500

    Update Javadcos

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"main"},{"l":"ui"},{"l":"utils"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"main"},{"l":"ui"}];updateSearchResults();
